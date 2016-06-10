0bd77508a5d90fadfce28108ada12a0d09befaa4
alexpeachey <alex.peachey@gmail.com>
2016-06-10 08:17:36 -0700

    Tighten steps

--- features/step_definitions/project_steps.js
+++ features/step_definitions/project_steps.js
@@ -1,14 +1,12 @@
 module.exports = function () {
+  let expect = this.expect;
+
   this.When(/^I view the project$/, function () {
-    return this.driver
-               .get('http://localhost:8080/')
+    return this.driver.get('http://localhost:8080/')
   })
 
   this.Then(/^I see '(.*)'$/, function (text) {
-    let expect = this.expect;
-    let header = this.driver
-                     .findElement({ css: 'h3' })
-                     .getText()
-    return expect(header).to.eventually.equal(text)
+    let header = this.driver.findElement({ css: 'h3' }).getText()
+    return this.expect(header).to.eventually.equal(text)
   })
 }
--- features/step_definitions/task_steps.js
+++ features/step_definitions/task_steps.js
@@ -12,16 +12,12 @@ module.exports = function () {
     }
     return this.driver
                .findElement({ css: 'h6.' + state })
-               .findElement({ css: 'button' })
-               .click()
+               .findElement({ css: 'button' }).click()
   })
 
   this.Then(/^I see the task '(.*)' under '(.*)'$/, function (text, heading) {
-    let expect = this.expect
     let state = heading.toLowerCase().split(' ').join('-')
-    let name = this.driver
-                   .findElement({ css: 'h6.' + state })
-                   .getText()
-    return expect(name).to.eventually.have.string(text)
+    let name = this.driver.findElement({ css: 'h6.' + state }).getText()
+    return this.expect(name).to.eventually.have.string(text)
   })
 }
