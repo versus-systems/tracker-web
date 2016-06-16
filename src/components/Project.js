891f3861c2aa56fe0ac2ed653753f8e39e00ef94
Joey Leung <joeyleung8@gmail.com>
2016-06-16 16:11:07 -0700

    Added Graph Colors and centered percentage

--- src/components/Project.js
+++ src/components/Project.js
@@ -22,22 +22,28 @@ class Project extends Component {
     let percentComplete = tasks.list.length > 0 ? tasks.complete/tasks.count*100 : 0
     let percentTodo = tasks.list.length > 0 ? tasks.todo/tasks.count*100 : 0
     let percentInProgress = tasks.list.length > 0 ? tasks.inProgress/tasks.count*100 : 0
-    
+
     return (
       <div>
         <h3>{name}</h3>
-        <div className="container">
-          <div className="col-md-4">
-            Complete: {tasks.complete}
-            <Circle percent={ percentComplete } strokeWidth="10" strokeColor="#3EC556" />
+        <div className='container'>
+          <div className='col-md-4 center-container'>
+            <p className='center-text'>Complete: {Math.floor(percentComplete)}</p>
+            <div className='col-md-6 col-centered'>
+              <Circle percent={ percentComplete } strokeWidth='10' strokeColor='#3EC556' />
+            </div>
           </div>
-          <div className="col-md-4">
-            In Progress: {tasks.todo} 
-            <Circle percent={ percentInProgress } strokeWidth="10" strokeColor="#3E74C5" />
+          <div className='col-md-4'>
+            <p className='center-text'>In Progress: {Math.floor(percentInProgress)}</p>
+            <div className='col-md-6 col-centered'>
+              <Circle percent={ percentInProgress } strokeWidth='10' strokeColor='#3E74C5' />
+            </div>
           </div>
-          <div className="col-md-4">
-            To Do: {tasks.inProgress}
-            <Circle percent={ percentTodo } strokeWidth="10" strokeColor="#C53E3E" />
+          <div className='col-md-4'>
+            <p className='center-text'>To Do: {Math.floor(percentTodo)}</p>
+            <div className='col-md-6 col-centered'>
+              <Circle percent={ percentTodo } strokeWidth='10' strokeColor='#C53E3E' />
+            </div>
           </div>
           <p>Tasks: {tasks.count}</p>
         </div>
