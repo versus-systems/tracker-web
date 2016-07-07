35953cf45cb5b0ad38f1b89aaccb54760ee19d98
alexpeachey <alex.peachey@gmail.com>
2016-07-07 09:34:40 -0700

    Switch to enzyme closes #6

--- features/step_definitions/project_steps.js
+++ features/step_definitions/project_steps.js
@@ -1,12 +1,13 @@
 module.exports = function () {
   let expect = this.expect;
 
-  this.When(/^I view the project$/, function () {
-    return this.driver.get('http://localhost:8080/')
+  this.When(/^I view the project$/, function (done) {
+    done()
   })
 
-  this.Then(/^I see '(.*)'$/, function (text) {
-    let header = this.driver.findElement({ css: 'h3' }).getText()
-    return this.expect(header).to.eventually.equal(text)
+  this.Then(/^I see '(.*)'$/, function (text, done) {
+    let header = this.wrapper.find('h3').text();
+    this.expect(header).to.equal(text);
+    done()
   })
 }
